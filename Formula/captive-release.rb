de09ff8451ee2df58f9c2423eedc263abf47f368
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-09-07 23:39:41 +0200

    🔖 Met à jour captive-release v0.3.0

--- Formula/captive-release.rb
+++ Formula/captive-release.rb
@@ -2,7 +2,7 @@ class CaptiveRelease < Formula
   desc "CLI de release semver pour les projets Captive"
   homepage "https://github.com/captive-studio/captive-release"
   url "https://rubygems.org/gems/captive-release-0.3.0.gem"
-  sha256 "PLACEHOLDER"
+  sha256 "c2f89c9d30885e14ae5ccb489bc4244e4f37a4ceb667611e4b7858f2180335a2"
   license "MIT"
 
   depends_on "ruby"
